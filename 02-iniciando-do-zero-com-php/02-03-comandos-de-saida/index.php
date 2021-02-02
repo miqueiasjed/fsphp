aef74d28a7ac7c76132476ce4fa26f8a57b47e56
Miqueias Mesquita <miqueias_jed@hotmail.com>
2021-02-02 20:24:52 -0300

    comandos de saida;

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,11 +7,37 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
+echo "<p>Olá mundo!"," ", "<span class='tag'>#BoraProgramar</span>","</p>";
+
+$hello = "Olá Mundo!";
+$code = "<span class='tag'>#BoraProgramar</span>";
+
+echo "<p>$hello</p>";
+echo '<p>$hello</p>';
+
+$day = "dia";
+echo "<p> Falta 1 $day para o evento!</p>";
+echo "<p> Falta 2 {$day}s para o evento!</p>";
+
+echo "<h3>{$hello}</h3>";
+echo "<h4>{$hello} {$code}</h4>";
+
+echo '<h3>'. $hello . '' . $code . "</h3>";
+?>
+
+<h4><?= $hello; ?> <?= $code; ?></h4>
+
+<?php
+
 
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
+print $hello;
+print $code;
+
+print "<h3>{$hello} {$code}</h3>";
 
 
 /**
@@ -19,11 +45,33 @@ fullStackPHPClassSession("print", __LINE__);
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$array = [
+    "company" => "UpInside",
+    "course" => "FSPHP",
+    "class" => "Comandos de saída"
+];
+
+print_r($array);
+
+echo "<pre>", print_r($array,true),"</pre>";
+
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
+
+$article = "<article><h1>%s</h1><p>%s</p></article>";
+$title = "{$hello} {$code}";
+$subtitle = "é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde
+ o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de
+  modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica,
+   permanecendo essencialmente inalterado. Se";
+
+printf($article, $title, $subtitle);
+echo sprintf($article, $title, $subtitle);
+
+
 
 
 /**
@@ -31,8 +79,16 @@ fullStackPHPClassSession("printf", __LINE__);
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
+$company = "<article><h1>Escola %s</h1><p>Curso <b>%s</b>, aula <b>%s</b></p></article>";
+
+vprintf($company, $array);
+
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump($array);
+
+var_dump($array, $hello, $company);

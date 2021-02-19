7723d9f26b6d61b82a9eb5896be6cba9b2230700
Miqueias Mesquita <miqueias_jed@hotmail.com>
2021-02-19 18:27:32 -0300

    operadores

--- 02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
+++ 02-iniciando-do-zero-com-php/02-05-operadores-na-pratica/index.php
@@ -8,11 +8,52 @@ echo fullStackPHPClassName("02.05 - Operadores na prática");
  */
 fullStackPHPClassSession("atribuição", __LINE__);
 
+$operatorA = 5;
+$operators = [
+  "a += 5" => ($operatorA += 5),
+  "a -= 5" => ($operatorA -= 5),
+  "a *= 5" => ($operatorA *= 5),
+  "a /= 5" => ($operatorA /= 5),
+];
+
+var_dump($operators);
+
+$incrementA = 5;
+$incrementB = 5;
+
+$increment = [
+    "pós-incremento" => $incrementA++,
+    "res-incremento" => $incrementA,
+    "pré-incremento" => ++$incrementA,
+    "pós-decremento" => $incrementA--,
+    "res-decremento" => $incrementA,
+    "pré-decremento" => --$incrementA,
+];
+
+var_dump($increment);
 
 /**
  * [ comparação ] https://php.net/manual/pt_BR/language.operators.comparison.php
  */
 fullStackPHPClassSession("comparação", __LINE__);
+
+$relatedA = 5;
+$relatedB = "5";
+$relatedC = 10;
+
+$related = [
+    "a == b" => ($relatedA == $relatedB),
+    "a === b" => ($relatedA === $relatedB),
+    "a != b" => ($relatedA != $relatedB),
+    "a !== b" => ($relatedA !== $relatedB),
+    "a > c" => ($relatedA > $relatedC),
+    "a < c" => ($relatedA < $relatedC),
+    "a >= b" => ($relatedA >= $relatedB),
+    "a >= c" => ($relatedA >= $relatedC),
+    "a <= c" => ($relatedA <= $relatedC),
+];
+
+var_dump($related);
 
 
 /**
@@ -20,8 +61,32 @@ fullStackPHPClassSession("comparação", __LINE__);
  */
 fullStackPHPClassSession("lógicos", __LINE__);
 
+$logicA = true;
+$logicB = false;
+$logic = [
+  "a && b" => ($logicA && $logicB),
+  "a || b" => ($logicA || $logicB),
+  "a" => ($logicA),
+  "! a" => (!$logicA),
+  "! b" => (!$logicB),
+];
+
+var_dump($logic);
+
 
 /**
  * [ aritiméticos ] https://php.net/manual/pt_BR/language.operators.arithmetic.php
  */
 fullStackPHPClassSession("aritiméticos", __LINE__);
+
+$calcA = 5;
+$calcB = 10;
+$calc = [
+    "a + b" => ($calcA + $calcB),
+    "a - b" => ($calcA - $calcB),
+    "a * b" => ($calcA * $calcB),
+    "a / b" => ($calcA / $calcB),
+    "a % b" => ($calcA % $calcB),
+];
+
+var_dump($calc);
